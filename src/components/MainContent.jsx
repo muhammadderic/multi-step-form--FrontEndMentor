c0e2533083c5eb53380e61021e7b347936b3d0f6
muhammadderic <muhammad.donny.e@gmail.com>
2023-09-05 22:52:08 +0700

    Create user interface step 1 active state

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -10,15 +10,18 @@ function MainContent() {
       <form>
         <div className="input-wrapper">
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" placeholder="e.g. Stephen King" />
+          <input className="content__input input-border" type="text" id="name" name="name" placeholder="e.g. Stephen King" />
+          <p className="error-input error-display-none">This field is required</p>
         </div>
         <div className="input-wrapper">
           <label htmlFor="email">Email Address</label>
-          <input type="text" id="email" name="email" placeholder="e.g. stephenking@lorem.com" />
+          <input className="content__input input-border" type="text" id="email" name="email" placeholder="e.g. stephenking@lorem.com" />
+          <p className="error-inpu error-display-none">This field is required</p>
         </div>
         <div className="input-wrapper">
           <label htmlFor="phone">Phone Number</label>
-          <input type="tel" id="phone" name="phone" placeholder="e.g. +1 234 567 890" />
+          <input className="content__input" type="tel" id="phone" name="phone" placeholder="e.g. +1 234 567 890" />
+          <p className="error-input error-display-none">This field is required</p>
         </div>
       </form>
     </div>
