b18de85f58294aef4e846f1123cac5dfe0d852db
muhammadderic <muhammad.donny.e@gmail.com>
2023-09-14 06:56:42 +0700

    Update sidebar status by step state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import MainContent from "./components/MainContent";
 import StepButtons from "./components/StepButtons";
 import Sidebar from "./components/Sidebar";
 import './styles/App.css';
+import { useSelector } from "react-redux";
 
 function App() {
+  const step = useSelector(state => state.step);
+
   return (
     <div id="app-container">
       <Sidebar />
-      {/* <MainContent title="Personal info" subtitle="Please provide your name, email address, and phone number." /> */}
-      {/* <MainContent title="Select your plan" subtitle="You have the option of monthly or yearly billing." /> */}
-      {/* <MainContent title="Pick add-ons" subtitle="Add-ons help enhance your gaming experience." /> */}
-      <MainContent title="Finishing up" subtitle="Double-check everything looks OK before confirming." />
+      {step === 0 && <MainContent title="Personal info" subtitle="Please provide your name, email address, and phone number." />}
+      {step === 1 && <MainContent title="Select your plan" subtitle="You have the option of monthly or yearly billing." />}
+      {step === 2 && <MainContent title="Pick add-ons" subtitle="Add-ons help enhance your gaming experience." />}
+      {step === 3 && <MainContent title="Finishing up" subtitle="Double-check everything looks OK before confirming." />}
       <StepButtons />
     </div>
   );
