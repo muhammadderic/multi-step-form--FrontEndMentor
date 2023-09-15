930fb4b64e57036dbf0e37e8ec7bdc51c666bf28
muhammadderic <muhammad.donny.e@gmail.com>
2023-09-15 20:29:56 +0700

    Add userPlanPrice state to userPlanReducer

--- src/components/userPlanReducer.js
+++ src/components/userPlanReducer.js
@@ -1,7 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  userPlanData: "",
+  userPlanData: {
+    arcade: {
+      userPlanTitle: "arcade",
+      userPlanPrice: 9,
+    },
+    advanced: {
+      userPlanTitle: "advanced",
+      userPlanPrice: 12,
+    },
+    pro: {
+      userPlanTitle: "pro",
+      userPlanPrice: 15,
+    }
+  },
+  userPlanSelected: {
+    userPlanTitle: "arcade",
+    userPlanPrice: 9,
+  },
   userPlanTime: "monthly",
 }
 
@@ -11,7 +28,25 @@ const userPlanReducer = createSlice({
   reducers: {
     updateUserPlan: (state, action) => {
       const { userPlan } = action.payload;
-      state.userPlanData = userPlan;
+      const userPlanData = { ...state.userPlanData };
+      const { arcade, advanced, pro } = userPlanData;
+      if (arcade.userPlanTitle === userPlan) {
+        state.userPlanSelected = { ...arcade };
+      } else if (advanced.userPlanTitle === userPlan) {
+        state.userPlanSelected = { ...advanced };
+      } else if (pro.userPlanTitle === userPlan) {
+        state.userPlanSelected = { ...pro };
+      }
+      // if (arcade.userPlanTitle === userPlan) {
+      //   state.userPlanSelected.userTitle = arcade.userPlanTitle;
+      //   state.userPlanSelected.userPrice = arcade.userPlanPrice;
+      // } else if (advanced.userPlanTitle === userPlan) {
+      //   state.userPlanSelected.userTitle = advanced.userPlanTitle;
+      //   state.userPlanSelected.userPrice = advanced.userPlanPrice;
+      // } else if (pro.userPlanTitle === userPlan) {
+      //   state.userPlanSelected.userTitle = pro.userPlanTitle;
+      //   state.userPlanSelected.userPrice = pro.userPlanPrice;
+      // }
     },
     updateUserPlanTime: (state, action) => {
       const { time } = action.payload;
